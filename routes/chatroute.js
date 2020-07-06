d446eb00c51efa19307baa7a874c743728f61e0a
Anil Yadav <ceo.sbycc@gmail.com>
2020-07-06 15:15:39 +0530

    updated

--- routes/chatroute.js
+++ routes/chatroute.js
@@ -2,9 +2,9 @@
 const express=require('express');
 const mongoose=require('mongoose')
 var router=express.Router();
-mongoose.Promise = global.Promise;
+// mongoose.Promise = global.Promise;
 
-mongoose.connect("mongodb+srv://sbycc2020:' + encodeURIComponent('sbycc@anilyadav21') + '@mongodb01-z7hcd.mongodb.net/c2chatbot2020?retryWrites=true", {useNewUrlParser: true,useUnifiedTopology:true}, (ignore, connection) => {
+mongoose.connect("mongodb+srv://sbycc2020:' + encodeURIComponent('sbycc@anilyadav21') + '@mongodb01-z7hcd.mongodb.net/chatbot2020?retryWrites=true", {useNewUrlParser: true,useUnifiedTopology:true}, (ignore, connection) => {
    console.log("mongo is connected");
    
 });
