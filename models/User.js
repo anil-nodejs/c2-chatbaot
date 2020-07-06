47e742c3af64483ae3c861319a60c311422b7e6b
Anil Yadav <ceo.sbycc@gmail.com>
2020-07-06 14:53:20 +0530

    updated

--- models/User.js
+++ models/User.js
@@ -6,6 +6,11 @@ var userSchema=new Schema({
     last_name: { type: String, "default": "" },
     lastAction: { type: String, "default": "" },
     createdDate: { type: Date, default: Date.now },
+    Mission_Green_Million: { type: Number, default: 0 },
+    whats_happening: { type: Number, default: 0 },
+    restart: { type: Number, default: 0 },
+
+
 });
 
 module.exports=mongoose.model("User",userSchema)
