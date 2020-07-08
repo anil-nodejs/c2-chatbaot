eec16a1fdbf7cecf8769a6de16abfe9b392c25b4
Anil Yadav <ceo.sbycc@gmail.com>
2020-07-08 11:05:31 +0530

    added

--- routes/chatroute.js
+++ routes/chatroute.js
@@ -4,7 +4,7 @@ const mongoose=require('mongoose')
 var router=express.Router();
 // mongoose.Promise = global.Promise;
 
-mongoose.connect("mongodb://aa_chatbot2020:7n!mT5e5dceAs_5@ds041432.mlab.com:41432/heroku_kx5wjtrh", {useNewUrlParser: true,useUnifiedTopology:true}, (ignore, connection) => {
+mongoose.connect("mongodb://aa_chatbot2020:7n!mT5e5dceAs_5@ds041432.mlab.com:41432/heroku_kx5wjtrh", {useNewUrlParser: true, useCreateIndex: true,useUnifiedTopology:true}, (ignore, connection) => {
     connection.onOpen();
 console.log("mongo is connected");
 
