5b021abd05a075b81336b6a5069655ead0b7b075
Anil Yadav <iter.anilyadav@gmail.com>
2020-07-08 21:11:24 +0530

    added

--- server.js
+++ server.js
@@ -1,24 +1,32 @@
-const express=require('express');
-const bodyParser=require('body-parser');
-const path=require('path');
-const app=express();
-const port =9000;
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+var mongoose = require('mongoose');
+const app = express();
+const port = 9000;
 
+//database
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://aa_chatbot2020:7n!mT5e5dceAs_5@ds041432.mlab.com:41432/heroku_kx5wjtrh", { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }, (ignore, connection) => {
+    connection.onOpen();
+    console.log("mongo is connected");
+
+});
 
 //route set path
-const chatRoute=require('./routes/chatroute');
-app.use('/',chatRoute);
 
-const marutiRoute=require('./routes/maruti');
-app.use('/maruti',marutiRoute);
+app.get('/', (req, res) => {
+    res.send("Working...");
+})
+const marutiRoute = require('./routes/maruti');
+app.use('/maruti', marutiRoute);
 // end path
 
 
-app.use(express.static(path.join(__dirname , 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
-app.listen(process.env.PORT || port,()=>
-{
-    console.log("chatbot server is runnig... at ",port);
+app.listen(process.env.PORT || port, () => {
+    console.log("chatbot server is runnig... at ", port);
 })
 
-module.exports=app;
+module.exports = app;
