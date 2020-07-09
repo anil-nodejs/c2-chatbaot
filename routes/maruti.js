6050caa6048912d49a3a895cc390d950690ba6d2
Anil Yadav <iter.anilyadav@gmail.com>
2020-07-09 14:48:15 +0530

    added

--- routes/maruti.js
+++ routes/maruti.js
@@ -111,6 +111,90 @@ app.get('/bot', (req, res) => {
 //post reques for bot
 
 
+// app.post('/bot', (req, res) => {
+//     let data = req.body;
+//     res.sendStatus(200);
+//     if (data.object === 'page') {
+//         data.entry.forEach(entry => {
+//             entry.messaging.forEach(event => {
+//                 if (event.sender == undefined || event.sender.id == undefined) {
+//                     return;
+//                 }
+//                 let senderID = event.sender.id;
+//                 if (event.delivery != undefined) {
+//                     deliveryReport(senderID);
+//                     return;
+//                 }
+//                 let message = event.message;
+//                 User.findOne({ fbid: senderID }).exec((err, user) => {
+//                     if (!err) {
+//                         // New User
+//                         if (user == null) {
+//                             console.log("new user");
+//                             let newUser = User();
+//                             newUser.fbid = senderID;
+//                             newUser.first_name = "";
+//                             newUser.last_name = "";
+//                             newUser.lastAction = "get started";
+//                             newUser.getstarted_clicks = 1;
+//                             console.log(newUser);
+//                             newUser.save((err, user) => {
+//                                 if (!err) {
+//                                     getData(senderID);
+//                                     // getstarted(senderID);
+//                                     console.log('====================================');
+//                                     console.log("femimrmikvrmf");
+//                                     console.log('====================================');
+
+//                                 }
+//                                 else {
+//                                     console.log("err 1");
+//                                     console.log(err);
+//                                 }
+//                             });
+//                         }
+//                         //Old user
+//                         else {
+//                             console.log("Old user");
+
+//                             if (user.first_name == "" || user.first_name == null) {
+//                                 getData1(senderID);
+//                             }
+
+//                             let eventAction = "";
+//                             if (event.message != undefined && event.message.quick_reply != undefined) {
+//                                 eventAction = event.message.quick_reply.payload;
+//                                 quickReplyRequest(senderID, eventAction);
+//                             }
+//                             else if (event.message != undefined && event.message.text != undefined) {
+//                                 textMessageRequest(senderID, event.message.text.toLowerCase(), user);
+//                             }
+//                             else if (event.message != undefined && event.message.attachments != undefined) {
+//                                 attachmentsRequest(senderID, event.message.attachments, user);
+//                             }
+//                             else {
+//                                 if (event.postback != undefined && event.postback.payload != undefined) {
+//                                     eventAction = event.postback.payload;
+//                                     postbackRequest(senderID, eventAction, user);
+//                                 }
+//                                 else {
+//                                     defaultMessage(senderID);
+//                                 }
+//                             }
+//                         }
+//                     }
+//                     else {
+//                         console.log("err");
+//                         console.log(err);
+//                     }
+//                 });
+
+//             });
+//         });
+//     }
+// });
+
+
 app.post('/bot', (req, res) => {
     let data = req.body;
     res.sendStatus(200);
@@ -137,15 +221,10 @@ app.post('/bot', (req, res) => {
                             newUser.last_name = "";
                             newUser.lastAction = "get started";
                             newUser.getstarted_clicks = 1;
-                            console.log(newUser);
                             newUser.save((err, user) => {
                                 if (!err) {
                                     getData(senderID);
-                                    // getstarted(senderID);
-                                    console.log('====================================');
-                                    console.log("femimrmikvrmf");
-                                    console.log('====================================');
-
+                                    /* getstarted(senderID); */
                                 }
                                 else {
                                     console.log("err 1");
@@ -156,7 +235,6 @@ app.post('/bot', (req, res) => {
                         //Old user
                         else {
                             console.log("Old user");
-
                             if (user.first_name == "" || user.first_name == null) {
                                 getData1(senderID);
                             }
@@ -167,7 +245,7 @@ app.post('/bot', (req, res) => {
                                 quickReplyRequest(senderID, eventAction);
                             }
                             else if (event.message != undefined && event.message.text != undefined) {
-                                textMessageRequest(senderID, event.message.text.toLowerCase(), user);
+                                textMessageRequest(senderID, event.message.text.toLowerCase());
                             }
                             else if (event.message != undefined && event.message.attachments != undefined) {
                                 attachmentsRequest(senderID, event.message.attachments, user);
