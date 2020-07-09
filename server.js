1cd97a041f8d0ae26a904dd50911f449ad64f847
Anil Yadav <iter.anilyadav@gmail.com>
2020-07-09 15:26:14 +0530

    added

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('/', (req, res) => {
 })
 const marutiRoute = require('./routes/m2.js');
 app.use('/maruti', marutiRoute);
-// end pathfre
+// end pathfredsde
 
 
 app.use(express.static(path.join(__dirname, 'public')));
