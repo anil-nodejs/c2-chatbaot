fb94cc78eca35f02702aa2c54d1a9cb01a4bda76
Anil Yadav <iter.anilyadav@gmail.com>
2020-07-09 11:24:30 +0530

    added

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = 9000;
 
 //database
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://aa_chatbot2020:7n!mT5e5dceAs_5@ds041432.mlab.com:41432/heroku_kx5wjtrh", { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }, (ignore, connection) => {
+mongoose.connect("mongodb://aa_chatbot2020:7n!mT5e5dceAs_5@ds041432.mlab.com:37357/heroku_f6wxmjw9", { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }, (ignore, connection) => {
     connection.onOpen();
     console.log("mongo is connected");
 
