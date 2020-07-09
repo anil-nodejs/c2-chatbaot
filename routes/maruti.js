f44f74ea4959bf4e44dc52e573ec7c6bf5ee0df6
Anil Yadav <iter.anilyadav@gmail.com>
2020-07-09 22:46:25 +0530

    uhuhui

--- routes/maruti.js
+++ routes/maruti.js
@@ -1041,9 +1041,9 @@ function postbackRequest(senderID, eventAction, user) {
                 }, 500);
             });
     }
-    // else {
-    //     defaultMessage(senderID);
-    // }
+    else {
+        defaultMessage(senderID);
+    }
 }
 
 function defaultMessage(senderID) {
