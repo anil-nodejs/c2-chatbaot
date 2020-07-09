42fe1748ee344298419f2db18c5d172e521bef1d
Anil Yadav <iter.anilyadav@gmail.com>
2020-07-09 13:11:31 +0530

    added

--- routes/maruti.js
+++ routes/maruti.js
@@ -139,7 +139,7 @@ app.post('/bot', (req, res) => {
                             newUser.getstarted_clicks = 1;
                             newUser.save((err, user) => {
                                 if (!err) {
-                                    // getData(senderID);
+                                    getData(senderID);
                                     getstarted(senderID);
                                 }
                                 else {
