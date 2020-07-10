a05e72257cb03088c861d980946a7892055d7f0a
Anil Yadav <iter.anilyadav@gmail.com>
2020-07-10 11:44:04 +0530

    added

--- routes/maruti.js
+++ routes/maruti.js
@@ -278,28 +278,28 @@ function getData(senderID) {
         });
 }
 
-// function getData1(senderID) {
-//     rp({
-//         uri: 'https://graph.facebook.com/v4.0/' + senderID,
-//         qs: {
-//             access_token: access_token,
-//             fields: 'first_name,last_name'
-//         },
-//         method: 'GET'
-//     })
-//         .then(response => {
-//             let data = JSON.parse(response);
-//             User.findOneAndUpdate(
-//                 { fbid: senderID },
-//                 { $set: { "first_name": data.first_name, "last_name": data.last_name } },
-//                 { upsert: false },
-//                 (err, user) => {
-//                 });
-//         })
-//         .catch(error => {
+function getData1(senderID) {
+    rp({
+        uri: 'https://graph.facebook.com/v4.0/' + senderID,
+        qs: {
+            access_token: access_token,
+            fields: 'first_name,last_name'
+        },
+        method: 'GET'
+    })
+        .then(response => {
+            let data = JSON.parse(response);
+            User.findOneAndUpdate(
+                { fbid: senderID },
+                { $set: { "first_name": data.first_name, "last_name": data.last_name } },
+                { upsert: false },
+                (err, user) => {
+                });
+        })
+        .catch(error => {
 
-//         });
-// }
+        });
+}
 
 function quickReplyRequest(senderID, eventAction) {
     if (eventAction == "pic_count_2") {
@@ -1046,9 +1046,9 @@ function postbackRequest(senderID, eventAction, user) {
     // }
 }
 
-function defaultMessage(senderID) {
-    sendItems(senderID, "");
-}
+// function defaultMessage(senderID) {
+//     sendItems(senderID, "");
+// }
 
 function sendTextMessage(recipientId, messageText) {
     let messageData = {
