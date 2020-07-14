5a0df28035f2523443fc5b5d69b59f3d4b5c956c
Anil Yadav <iter.anilyadav@gmail.com>
2020-07-14 11:08:39 +0530

    datbase updated

--- config/database.js
+++ config/database.js
@@ -1,6 +1,11 @@
-db_Link = 'mongodb+srv://sbycc2020:' + encodeURIComponent('sbycc@anilyadav21') + '@mongodb01-z7hcd.mongodb.net/c2chatbot?retryWrites=true';
+// db_Link = 'mongodb+srv://sbycc2020:' + encodeURIComponent('sbycc@anilyadav21') + '@mongodb01-z7hcd.mongodb.net/c2chatbot?retryWrites=true';
+// module.exports =
+// {
+//     mongoDB_URL: db_Link
+// }
+
+db_Link = 'mongodb://heroku_19sv2vb4:heroku_19sv2vb4@ds021182.mlab.com:21182/heroku_19sv2vb4';
 module.exports =
 {
     mongoDB_URL: db_Link
 }
-
